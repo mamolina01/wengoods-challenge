a64694ba9f0702d0bc5051ba26cce22c2f8de685
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-27 23:26:38 -0300

    add header

---
+++ src/app/components/ui/header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss'
+
+export const Header = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Wengoods Challenge</h1>
+    </div>
+  )
+}
