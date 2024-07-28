1e89a665c48f1fa36cfd60aea64fa7279acb3f8b
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-28 01:37:05 -0300

    layout and homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={monserrat.className}>
+      <body className={`${monserrat.className}`}>
         <Header />
         {children}
       </body>
